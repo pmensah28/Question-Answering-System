3b81e1a89c9353143bb3aab3bc1e2a5d41a2ca1a
Prince Mensah <princemensaheyram@gmail.com>
2023-09-29 23:01:40 +0000

    error corrected in file

--- app/model.py
+++ app/model.py
@@ -1,22 +1,24 @@
+# Importing libraries
 from sentence_transformers import SentenceTransformer
 import pandas as pd
-import numpy as np
 
-# Load the model
+# Loading the the CSV file
+csv_file = '/home/prince/Dropbox/Question-Answering-System/docs/passage_metadata.csv' # Replace with the actual path
+df = pd.read_csv(csv_file)
+
+# Load the SentenceTransformer model
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
-# Load the passages and metadata CSV file
-df = pd.read_csv('/home/prince/Dropbox/Question-Answering-System/docs/passage_metadata.csv')
-
-# Generate embeddings for each passage
-embeddings = model.encode(df['Passage'].tolist())
-# Displaying the first 2 embeddings for inspection
+# Generate embeddings for each passages
+embeddings = model.encode(df['Passage'].tolist(), convert_to_tensor=True)
+# # Displaying the first 2 embeddings for inspection
 print(embeddings[:2])
-
+# Convert embeddings tensor to list
+embeddings_list = embeddings.tolist()
 # Add the embeddings to the DataFrame
-df['Embedding'] = embeddings.tolist()
+df['Embedding'] = embeddings_list
 
-# Save the updated DataFrame to a new CSV file
+# Save the updated DataFrame with embeddings to a new CSV file
 df.to_csv('/home/prince/Dropbox/Question-Answering-System/docs/passage_metadata_emb.csv', index=False)
 
-print("Embeddings generated successfully and saved to passage_metadata_emb.csv.")
+print("Embeddings generated, added to the dataframe and saved to passage_metadata_emb.csv.")
