568389b4c4cc8946d6cbe85e94cc385d92790bd5
Prince Mensah <princemensaheyram@gmail.com>
2023-09-30 07:04:38 +0000

    Set up elasticSearch index to store the preprocessed and embedded data

---
+++ app/indexing.py
@@ -0,0 +1,38 @@
+# Import libraries
+from elasticsearch import Elasticsearch, helpers
+import pandas as pd
+import json
+
+# Connect to the ElasticSearch cluster
+# es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+# Verify that the connection is established
+if es.ping():
+    print("Connected to ElasticSearch.")
+else:
+    print("Could not connect to ElasticSearch.")
+    exit()
+
+# Load the data from the CSV file
+csv_file = '/home/prince/Dropbox/Question-Answering-System/docs/passage_metadata_emb.csv'
+df = pd.read_csv(csv_file)
+
+# Prepare the data for bulk indexing
+actions = [
+    {
+        "_index": "passages",
+        "_source": {
+            "passage": row['Passage'],
+            "metadata": json.loads(row['Metadata']),
+            "embedding": row['Embedding'][1:-1].split(', ')  # Convert string representation of list to actual list
+        }
+    }
+    for _, row in df.iterrows()
+]
+
+# Create an index
+es.indices.create(index="passages", ignore=400)
+
+# Now let's perform bulk indexing
+helpers.bulk(es, actions)
+
+print("Data indexed successfully.")
